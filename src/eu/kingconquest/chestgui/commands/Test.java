c03956617250629f6b4962d6b2dd487625002f74
Liquid <thomas.lundqvist.1991@gmail.com>
2016-09-03 21:42:50 +0200

    Version 0.1

---
+++ src/eu/kingconquest/chestgui/commands/Test.java
@@ -0,0 +1,35 @@
+package eu.kingconquest.chestgui.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import eu.kingconquest.chestgui.Main;
+import eu.kingconquest.chestgui.core.TpGui;
+
+public class Test implements CommandExecutor{
+	private static Player p;
+	private static TpGui tg;
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label,
+			String[] args){
+
+		if (sender instanceof Player)
+			p = (Player) sender;
+			tg = new TpGui();
+			open(p);
+		return false;
+	}
+	
+	private static void open(Player p){
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+            	tg.open(p);
+            }
+        }.runTaskLater(Main.getInstance(), 1);
+	}
+}
